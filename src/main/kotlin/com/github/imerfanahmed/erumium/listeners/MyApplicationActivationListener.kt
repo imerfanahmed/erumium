f9154d35c7db68268b87fb06557cb8bf04a0784a
GitHub Action <action@github.com>
2024-03-13 14:27:59 +0000

    Template cleanup

--- src/main/kotlin/com/github/imerfanahmed/erumium/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/imerfanahmed/erumium/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.imerfanahmed.erumium.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
